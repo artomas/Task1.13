16270985fd1a22e6ae02779cfc34744f29b056af
artomas <mosalovartem@yandex.ru>
2025-05-29 11:15:37 +0300

    Initial commit

---
+++ FileManagerApp/Models/FileItem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FileManagerApp.Models;
+
+public enum FileCategory
+{
+    File,
+    Directory
+}
+
+public class FileItem : INotifyPropertyChanged
+{
+    private string _name;
+    private string _path;
+
+    public string Name
+    {
+        get => _name;
+        private set
+        {
+            _name = value;
+            OnPropertyChanged(nameof(Name));
+        }
+    }
+
+    public FileCategory Category { get; }
+    public long Size { get; }
+    
+    public string Path
+    {
+        get => _path;
+        private set
+        {
+            _path = value;
+            OnPropertyChanged(nameof(Path));
+        }
+    }
+
+    public FileItem(string name, FileCategory category, long size, string path)
+    {
+        Name = name;
+        Category = category;
+        Size = category == FileCategory.Directory ? 0 : size;
+        Path = path;
+    }
+
+    public FileItem Copy(string newPath) => new(Name, Category, Size, newPath);
+
+    public void Move(string newPath)
+    {
+        if (string.IsNullOrWhiteSpace(newPath))
+            throw new ArgumentException("Path cannot be empty");
+        
+        Path = newPath;
+    }
+
+    public void Rename(string newName)
+    {
+        if (string.IsNullOrWhiteSpace(newName))
+            throw new ArgumentException("Name cannot be empty");
+        
+        Name = newName;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ FileManagerApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Input;
+using FileManagerApp.Models;
+
+namespace FileManagerApp.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private FileItem _currentFile;
+    public FileItem CurrentFile
+    {
+        get => _currentFile;
+        set
+        {
+            _currentFile = value;
+            OnPropertyChanged(nameof(CurrentFile));
+            OnPropertyChanged(nameof(CurrentFile.Name));
+            OnPropertyChanged(nameof(CurrentFile.Path));
+        }
+    }
+
+    public ICommand CopyCommand { get; }
+    public ICommand MoveCommand { get; }
+    public ICommand RenameCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        CurrentFile = new FileItem("document.txt", FileCategory.File, 1024, "C:\\Temp\\original.txt");
+
+        CopyCommand = new RelayCommand(_ =>
+        {
+            var newPath = $"C:\\Temp\\copy_of_{CurrentFile.Name}";
+            CurrentFile = CurrentFile.Copy(newPath);
+        });
+
+        MoveCommand = new RelayCommand(_ =>
+        {
+            var newPath = $"C:\\Temp\\moved\\{CurrentFile.Name}";
+            CurrentFile.Move(newPath);
+            CurrentFile = CurrentFile;
+        });
+
+        RenameCommand = new RelayCommand(_ =>
+        {
+            CurrentFile.Rename($"renamed_{CurrentFile.Name}");
+            CurrentFile = CurrentFile;
+        });
+    }
+}
